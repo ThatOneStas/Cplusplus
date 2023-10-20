d12a0a61f24b6744feaf75900b42a6139504f29d
ThatOneStas <138216528+ThatOneStas@users.noreply.github.com>
2023-10-20 18:24:03 +0300

    20.10

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*cout << "Hello World!" << endl << "Name" << endl;
+    int x = 0;
+    cout << "Input number: ";
+    cin >> x;
+    cout << endl << x << endl;
+    cout << sizeof(int) << endl;*/
+
+    float f = 5.14444414f;
+    double d = 5.24412142;
+    cout << f << endl;
+    cout << d << endl;
+
+    char c = 'a';
+    cout << c << endl;
+
+    string str = "ssttffgg";
+    cout << str << endl;
+
+    long l = 424;
+    cout << l << endl;
+
+    unsigned int i = -5;
+    cout << i << endl;
+}
+
